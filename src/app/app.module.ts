0f79070fb591e3d439d33064743976160dfe3568
lluanagabrieli <luanagabrieli4599@hotmail.com>
2023-12-09 08:29:25 -0300

    adiciona novos modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
-import { SucessoCadastroComponent } from './sucesso-cadastro/sucesso-cadastro.component';
-import { FormsModule } from '@angular/forms';
-import { MensagemComponent } from './components/mensagem/mensagem.component';
-import { MaiorIdadeDirective } from './directives/maior-idade.directive';
-import { MudarCorDirective } from './directives/mudar-cor.directive';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { CadastroComponent } from "./cadastro/cadastro.component";
+import { SucessoCadastroComponent } from "./sucesso-cadastro/sucesso-cadastro.component";
+import { FormsModule } from "@angular/forms";
+import { MensagemComponent } from "./components/mensagem/mensagem.component";
+import { MaiorIdadeDirective } from "./directives/maior-idade.directive";
+import { MudarCorDirective } from "./directives/mudar-cor.directive";
+import { ValidandoCepDirective } from './directives/validando-cep.directive';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, CadastroComponent, SucessoCadastroComponent, MensagemComponent, MaiorIdadeDirective, MudarCorDirective],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CadastroComponent,
+    SucessoCadastroComponent,
+    MensagemComponent,
+    MaiorIdadeDirective,
+    MudarCorDirective,
+    ValidandoCepDirective,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
