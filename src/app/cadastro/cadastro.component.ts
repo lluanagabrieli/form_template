95b640f8036ad5ccb1246708bf07a7bf05713965
lluanagabrieli <luanagabrieli4599@hotmail.com>
2023-12-10 18:57:58 -0300

    :sparkles: faz a injeção de dependência do service

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,26 +1,50 @@
-import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { ConsultaCepService } from "./../service/consulta-cep.service";
+import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
 
-import{ NgForm } from '@angular/forms';
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-cadastro',
-  templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html",
+  styleUrls: ["./cadastro.component.css"],
 })
 export class CadastroComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private consultacepService: ConsultaCepService
+  ) {}
 
-  constructor(private router: Router) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  cadastrar(form: NgForm){
-    if(form.valid){
-      this.router.navigate(['./sucesso'])
-    }else{
-      alert('Formulário inválido!')
+  cadastrar(form: NgForm) {
+    if (form.valid) {
+      this.router.navigate(["./sucesso"]);
+    } else {
+      alert("Formulário inválido!");
     }
     //console.log(form.controls)
+  }
+
+  consultaCEP(evento: any, f: NgForm) {
+    // captando o valor que foi colocado no input
+    const cep = evento.target.value;
+
+    if (cep !== "") {
+      this.consultacepService.getConsultaCep(cep).subscribe((resultado) => {
+        console.log(resultado);
+        this.populandoEndereco(resultado, f);
+      });
+    }
+  }
+
+  populandoEndereco(dados: any, form: NgForm) {
+    form.form.patchValue({
+      endereco: dados.logradouro,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      estado: dados.uf,
+    });
   }
 }
